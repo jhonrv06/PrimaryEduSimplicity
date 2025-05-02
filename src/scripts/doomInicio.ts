e0aabcd4f0ec04ea3c41ff87d232611ab805aaa0
Jhon Rivera <19398401+jhonrv06@users.noreply.github.com>
2025-05-02 01:14:22 -0500

    servir archivos .ts como componentes de astro
    para podder servir archivos .ts como componentes
    de astro es necesario importar el archivos ts
    con la ruta absoluta from '/src/scripts/doomInicio.ts';
    que apunte a la carpeta src/scripts. en resumen lo que
    se de hacer para mantener la lógica TypeScript
    en src/scripts/doomInicio.ts. es necesario
    
    Importarla desde un componente .astro usando la ruta
    absoluta que Vite expone:
    
    from '/src/scripts/doomInicio.ts';
    Ejecutarla dentro de un
    <script type="module" is:inline>
    para que corra en el navegador cuando el DOM esté listo.

---
+++ src/scripts/doomInicio.ts
@@ -0,0 +1,25 @@
+/*
+export function eventSliderMoov(){
+
+    const BUTTONDERECHA = document.querySelector("#button1");
+    const BUTTONDIZQUERDA = document.querySelector("#button2")
+    const SLIDER = document.querySelector(".slider");
+    const slideWidth = (SLIDER as HTMLElement).offsetWidth;
+
+    if(BUTTONDERECHA && BUTTONDIZQUERDA && SLIDER){
+
+        BUTTONDERECHA.addEventListener("click", ()=>{
+            SLIDER.scrollBy({ left: slideWidth, behavior: 'smooth' });
+        });
+    
+        BUTTONDIZQUERDA.addEventListener("click", ()=>{
+            SLIDER.scrollBy({ left: -slideWidth, behavior: 'smooth' });
+        });
+    }
+}*/
+
+export function eventSliderMoov(){
+
+    console.log("hola");
+}
+
