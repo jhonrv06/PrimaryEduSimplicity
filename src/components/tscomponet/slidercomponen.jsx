76e369b6b3bebdc79c99152349ee48ddc4e055d0
Jhon Rivera <19398401+jhonrv06@users.noreply.github.com>
2025-05-04 22:12:29 -0500

    Se implementa preact para el slider
    se eliminan los scripts de astro y se implementa
    un nuevo componente.jsx donde se utiliza ofsetWith
    para mover el slider.

---
+++ src/components/tscomponet/slidercomponen.jsx
@@ -0,0 +1,60 @@
+import { useEffect, useState, useRef} from  'preact/hooks'
+import  './sliderstyle.css';
+import left from '../../assets/inicio/leftbutton.svg'
+import Right from '../../assets/inicio/rightbutton.svg'
+import Timetolearn from '../../assets/inicio/Timetolearn.svg'
+import imagensilder from '../../assets/inicio/Imagen2.png'
+
+export default  function SliderComponent(){
+    const elementRef = useRef(null);
+    const [currenWith, setCurent] = useState(0);
+    
+    
+    const movRigth = () =>{
+        elementRef.current.scrollBy({ 
+            left: currenWith, 
+            behavior: 'smooth' })
+    };
+
+    const movLeft = () =>{
+        elementRef.current.scrollBy({ 
+            left: -currenWith, 
+            behavior: 'smooth' })
+    };
+
+    useEffect(() =>{
+        setCurent(elementRef.current.offsetWidth)
+    },[])
+
+    return(
+        <>
+        <article class="slider" ref={elementRef}>
+            <div class="slider__container" >
+            <section class="slider__section" id="slider1">
+				<article class="slider__text">
+					<h2>Bienvenido</h2>
+					<p>En <span class="container__span --Primary">Primary </span><span class="container__span --Edu">Edu </span><span class="container__span --simplicity">Simplicity</span> Creamos materiales que motivan a los niños y facilitan tu trabajo</p> <button class="button__span"> Unete ahora</button>
+                </article>
+                <img src={imagensilder.src} alt="Primary" />
+			</section>
+			<section class="slider__section" id="slider2">
+				<article class="slider__text">
+					<p>Docentes ayudando a docentes. Comparte ideas y crece junto a otros educadores.</p> 
+					<a href="/tusClases">Explora nuestros materiales</a>
+				</article>
+                <img src={Timetolearn.src} alt="Tiempo de aprender" />
+			</section>
+            </div>
+        </article>
+        <section class="buttons__container">
+            <button onClick={movLeft} class="button__event">
+                <img src={left.src} alt="Mover slider" />
+            </button>
+            <button onClick={movRigth} class="button__event">
+                <img src={Right.src} alt="Mover slider" />
+            </button>
+        </section>
+        </>
+    );
+}
+
