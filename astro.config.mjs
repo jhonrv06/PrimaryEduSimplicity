377d18d2e201565145db9608825ae8a0ff4a65a0
Jhon Rivera <19398401+jhonrv06@users.noreply.github.com>
2025-05-18 09:46:41 -0500

    prueba 13

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
-
+import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
-    output: 'static',
+    output: 'server',
+    adapter: vercel({
+        webAnalytics: {
+        enabled: true,
+    },
+    }),
     integrations: [preact()],
 });
 
