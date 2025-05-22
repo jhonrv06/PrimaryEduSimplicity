c373a781cb07f57d2f13773a188f96e7efbb49d5
Jhon Rivera <19398401+jhonrv06@users.noreply.github.com>
2025-05-22 00:31:32 -0500

    Se crea lógica para renderizar daos en componente tus clases

--- src/lib/WorpressH.ts
+++ src/lib/WorpressH.ts
@@ -32,19 +32,29 @@ export async function getGrados() {
     }
 }
 
+export async function getPeriodos() {
+    try{
+        const response = await fetch(`${WP}${routeApi}periodo`);
+        const dataGrado = await response.json();
+
+        const getValueGrados = dataGrado.map( grado =>{
+            const  {name, id} = grado;
+            return {name, id}
+        })
+        return getValueGrados;
+
+    }catch(error){
+        console.log(`Error al obtener los datos ${error}`)
+    }
+}
+
+
 
 export async function getMedia(dataG: string) {
     try{
         const response = await fetch(`${WP}${routeApi}media?mime_type=application/pdf&grado=${dataG}`);
         const dataMedia = await response.json();
-
-        const getDataMedia = dataMedia.map( data =>{
-        
-            const {link, title } = data;
-
-            return{link, title}
-        })
-        return getDataMedia
+        return dataMedia
 
     }catch(error){
         console.log(`Error al obtener los datos ${error}`)
