c4de6aac93e33a2c2cd436f41f30c129bb91f437
Jhon Rivera <19398401+jhonrv06@users.noreply.github.com>
2025-06-09 22:25:07 -0500

    Se agrega pdf worker.min.mjs
    Para poder utilizar el worker es necesario acceder
    a este desde la carpeta public, por lo que se agrega
    el worker al proyecto y se modifica el componente

--- src/components/tscomponet/GuiasPdf.tsx
+++ src/components/tscomponet/GuiasPdf.tsx
@@ -1,29 +1,51 @@
 import { useEffect, useState, useRef} from  'preact/hooks'
+//import * as pdfjsLib from 'pdfjs-dist';
 
-export default function Guias(){
+import { getDocument, GlobalWorkerOptions  } from "pdfjs-dist/legacy/build/pdf.mjs";
 
+
+
+const urlPdf = 'https://primaryedusimplicity.com/wp-content/uploads/2025/05/MONTHS-OF-THE-YEAR.pdf';
+
+export  default  function Guias(){
+    
     const canva = useRef<HTMLCanvasElement>(null);
-
     
-    
-
+    console.log("componente funcionando")
+   
     useEffect(() =>{
-        const canvasCurrent = canva.current;
-        const canvasContex = canvasCurrent?.getContext('2d');
+        async function renderPdf() {
+        GlobalWorkerOptions.workerSrc = "/pdf.worker.min.mjs";
 
-        canvasContex.fillStyle = 'blue';
-        canvasContex.fillRect(50, 50, 100, 75);
+        try{
+            const loadingTask =  getDocument(urlPdf);
+            const pdf = await loadingTask.promise;
+            const page = await pdf.getPage(1);
+            const viewport = page.getViewport({ scale: 0.5 });
+            const canvasCurrent = canva.current;
 
+            if (!canvasCurrent) return;
+            const contex = canvasCurrent?.getContext('2d');
+            canvasCurrent.height = viewport.height;
+            canvasCurrent.width = viewport.width;
+
+            await page.render({ canvasContext: contex!, viewport }).promise;
+          
+            console.log(contex);
+            return
+
+        }catch(error){
+            console.log(`Error al obtener el PDF ${error}`)
+        }
+        
+    }
+     renderPdf()
+    
         //console.log(canvasCurrent);
     },[])
-   
 
-
-
-    
      return(
         <>
-        
             <canvas width={"40"} height={"40"} ref={canva} style={{ border: '1px solid black' }}>
                 
             </canvas>
