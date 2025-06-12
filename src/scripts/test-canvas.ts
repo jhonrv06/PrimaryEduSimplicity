b121567a879e1e1a22848a224e6f304ff0dda546
Jhon Rivera <19398401+jhonrv06@users.noreply.github.com>
2025-06-11 22:31:42 -0500

    Se crea script utilizando pdf2pic
    Esto lo hago debido a que requiero conveertir
    la primera pagina de los PDF en imagenes para
    presentarlos como vista previa ya que hacerlo con
    canvas es muy lento y consume muchos recursos

---
+++ src/scripts/test-canvas.ts
@@ -0,0 +1,50 @@
+import fs from 'fs';
+import path from 'path';
+import { fromPath } from 'pdf2pic';
+
+
+const pdfUrl = 'https://primaryedusimplicity.com/wp-content/uploads/2025/05/MONTHS-OF-THE-YEAR.pdf';
+const localPdfPath = './temp/months.pdf';
+
+console.log("Iniciando descarga y conversión de PDF a imagen...");
+
+async function descargarYConvertirPDF() {
+
+    try {
+        const response = await fetch(pdfUrl);
+        if (!response.ok) throw new Error(`Error al descargar PDF: ${response.statusText}`);
+
+        const arrayBuffer = await response.arrayBuffer();
+        const buffer = Buffer.from(arrayBuffer);
+
+        fs.mkdirSync('./temp', { recursive: true });
+
+        fs.writeFileSync(localPdfPath, buffer);
+        console.log("✅ PDF descargado y guardado localmente.");
+
+        const outputDir = './public/thumbnails';
+        fs.mkdirSync(outputDir, { recursive: true });
+
+        const convert = fromPath(localPdfPath, {
+        density: 150,
+        saveFilename: "months",
+        savePath: outputDir,
+        format: "png",
+        width: 600,
+        height: 800
+        });
+
+        console.log("🖼️ Convirtiendo PDF a imagen...");
+        await convert(1); // solo la primera página
+        console.log("✅ Imagen generada correctamente en 'public/thumbnails/months.png'");
+
+    }catch (error) {
+        console.error("❌ Error durante la descarga o conversión:", error);
+    }
+    
+}
+
+
+
+descargarYConvertirPDF();
+
