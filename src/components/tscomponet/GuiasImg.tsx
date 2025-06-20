3da2323971624635c40f826ed9bca45dd607b6c1
Jhon Rivera <19398401+jhonrv06@users.noreply.github.com>
2025-06-19 22:59:11 -0500

    Se agregan estilos a las tarjetas y se cera api para descargar pdf de las guías

--- src/components/tscomponet/GuiasImg.tsx
+++ src/components/tscomponet/GuiasImg.tsx
@@ -3,16 +3,16 @@ import  './styles/img.css';
 
 type Props = {
   urlPdf: string;
+  linkPdf: string;
 };
  const urlPdf2 = `/thumbnails/GUÍA 4 TRANSICIÓN-PRIMER PERIODO.png`;
 
-export  default  function GuiasImg( { urlPdf }: Props){
+export  default  function GuiasImg( { urlPdf, linkPdf }: Props){
 
   const urlPdfRender = `/thumbnails/${urlPdf}.png`
 
      return(
         <>
-            <img className={"contenImg"} src={urlPdfRender} width={"200px"} height={"auto"}  alt=""/>
-
+            <a href={linkPdf} target={"_blank"} ><img className={"contenImg"} src={urlPdfRender} width={"200px"} height={"auto"}  alt=""/></a>
         </>
 )}
