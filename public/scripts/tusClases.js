f90d07a8c1bb953e667f10e958d4cc935706c567
Jhon <19398401+jhonrv06@users.noreply.github.com>
2025-09-01 20:56:51 -0500

    Se crean estilos para personalizar el menu de guias y habilidades

--- public/scripts/tusClases.js
+++ public/scripts/tusClases.js
@@ -1,5 +1,7 @@
 
-const guies = document.querySelector(".Habilidades");
+const guias = document.querySelector(".guiasConten");
+const Habilidades = document.querySelector(".habilidadesConten");
+
 const guiesButton = document.querySelectorAll(".buttton");
 
 guiesButton.forEach(element => {
@@ -7,6 +9,15 @@ guiesButton.forEach(element => {
         const elementValue = e.target;
         const elementId = elementValue.id;
         
+        if(elementId == "Habilidades"){
+            Habilidades.classList.remove("oculto")
+            Habilidades.classList.add("visible")
+            guias.classList.add("oculto")
+        }else{
+            guias.classList.remove("oculto")
+            Habilidades.classList.remove("visible")
+            Habilidades.classList.add("oculto")
+        }
         
     })
 });
